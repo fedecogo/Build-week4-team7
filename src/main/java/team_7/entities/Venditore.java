e1c1c923ca9a23719f36cf6d69b37bdf717aab7a
Angela Prestano <angiprestano99@gmail.com>
2023-12-18 16:39:08 +0100

    aggiuntaClassVenditore

---
+++ src/main/java/team_7/entities/Venditore.java
@@ -0,0 +1,54 @@
+package team_7.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "venditori")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="categoria")
+
+public abstract class Venditore {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nome;
+    private String località;
+
+    public Venditore() {
+
+    }
+
+    public Venditore(String nome, String località) {
+        this.nome = nome;
+        this.località = località;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalità() {
+        return località;
+    }
+
+    public void setLocalità(String località) {
+        this.località = località;
+    }
+
+    @Override
+    public String toString() {
+        return "Venditore{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", località='" + località + '\'' +
+                '}';
+    }
+}
