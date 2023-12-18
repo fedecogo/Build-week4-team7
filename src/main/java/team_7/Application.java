b45396ff77d681ca825de5d3b6bcbc6c171e615c
MoEzwawi <mezwawi96@gmail.com>
2023-12-18 14:06:15 +0100

    adds er diagram

--- src/main/java/team_7/Application.java
+++ src/main/java/team_7/Application.java
@@ -2,10 +2,12 @@ package team_7;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import team_7.functionalities.DateParser;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("galileo_express");
@@ -13,6 +15,11 @@ public class Application {
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
+        LocalDate data1 = LocalDate.of(2020,4,6);
+        LocalDate data2 = DateParser.parseDateForItaly("06/04/2020");
+
+        System.out.println(data1);
+        System.out.println(data2);
 
     }
 }
