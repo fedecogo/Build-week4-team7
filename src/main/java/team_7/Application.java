4dfca205083dbbcf66cfadf74beb01a500b4f44e
Carmen Caniglia <carmencaniglia@gmail.com>
2023-12-21 17:21:19 +0100

    biglietti

--- src/main/java/team_7/Application.java
+++ src/main/java/team_7/Application.java
@@ -30,30 +30,53 @@ public class Application {
         TrattaDAO trattaDAO = new TrattaDAO(em);
         ViaggioDAO viaggioDAO = new ViaggioDAO(em);
 
+        PuntoVendita tabacchino = puntoVenditaDAO.findById(17);
+        Biglietto aldoBg = new Biglietto(LocalDate.now(),tabacchino,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg);
+        Biglietto aldoBg1 = new Biglietto(LocalDate.now(),tabacchino,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg1);
+        Biglietto aldoBg2 = new Biglietto(LocalDate.now(),tabacchino,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg2);
+        Biglietto aldoBg3 = new Biglietto(LocalDate.now(),tabacchino,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg3);
+        Biglietto aldoBg4 = new Biglietto(LocalDate.now(),tabacchino,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg4);
 
-        //MezzoDiTrasporto busFromDb = mezzoDiTrasportoDAO.findById(104);
-        //Manutenzione m1 = new Manutenzione(LocalDate.now(), "sono io il problema" , busFromDb);
-         Manutenzione m1 = manutenzioneDAO.findById(108);
-         m1.setDataFine(LocalDate.now());
-     //   m1.setDataFine(LocalDate.now());
-        manutenzioneDAO.save(m1);
+        PuntoVendita edicola = puntoVenditaDAO.findById(18);
+        Biglietto aldoBg5 = new Biglietto(LocalDate.now(),edicola,TipoTratta.MEDIA);
+        bigliettoDAO.save(aldoBg5);
+        Biglietto aldoBg6 = new Biglietto(LocalDate.now(),edicola,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg6);
+        Biglietto aldoBg7 = new Biglietto(LocalDate.now(),edicola,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg7);
+        Biglietto aldoBg8 = new Biglietto(LocalDate.now(),edicola,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg8);
+        Biglietto aldoBg9 = new Biglietto(LocalDate.now(),edicola,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg9);
 
+        PuntoVendita biglietteria = puntoVenditaDAO.findById(19);
+        Biglietto aldoBg51 = new Biglietto(LocalDate.now(),biglietteria,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg51);
+        Biglietto aldoBg62 = new Biglietto(LocalDate.now(),biglietteria,TipoTratta.MEDIA);
+        bigliettoDAO.save(aldoBg62);
+        Biglietto aldoBg73 = new Biglietto(LocalDate.now(),biglietteria,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg73);
+        Biglietto aldoBg84 = new Biglietto(LocalDate.now(),biglietteria,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg84);
+        Biglietto aldoBg95 = new Biglietto(LocalDate.now(),biglietteria,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg95);
 
-
-
-
-
-
-
-
-
-
-        /*System.out.println("Ciao inserisci id utente");
-        long idUt = Long.parseLong(sc.nextLine());
-        Utente userFromDB = utenteDao.findById(idUt);
-
-        if(userFromDB != null) System.out.println("Benvenuto!"+userFromDB);*/
-
+        PuntoVendita distributore = puntoVenditaDAO.findById(20);
+        Biglietto aldoBg512 = new Biglietto(LocalDate.now(),distributore,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg512);
+        Biglietto aldoBg623 = new Biglietto(LocalDate.now(),distributore,TipoTratta.MEDIA);
+        bigliettoDAO.save(aldoBg623);
+        Biglietto aldoBg734 = new Biglietto(LocalDate.now(),distributore,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg734);
+        Biglietto aldoBg845 = new Biglietto(LocalDate.now(),distributore,TipoTratta.LUNGA);
+        bigliettoDAO.save(aldoBg845);
+        Biglietto aldoBg956 = new Biglietto(LocalDate.now(),distributore,TipoTratta.BREVE);
+        bigliettoDAO.save(aldoBg956);
 
         em.close();
         emf.close();
